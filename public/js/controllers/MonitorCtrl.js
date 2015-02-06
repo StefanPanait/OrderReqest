4fe8acb89ee67652b25cd56d4c10730f1fa374c2
StefanPanait <stefanapanait@gmail.com>
2015-02-06 00:47:55 -0500

    new relic monitoring graph

--- public/js/controllers/MonitorCtrl.js
+++ public/js/controllers/MonitorCtrl.js
@@ -2,7 +2,8 @@
 angular.module('orderRequest')
     .controller("MonitorCtrl", ['$scope',
         function($scope) {
-            console.log("loaded controller");
+            $scope.chartRefreshTime = 60;
+            var ctx = $("#myChart").get(0).getContext("2d");
 
             var options = {
 
@@ -48,23 +49,24 @@ angular.module('orderRequest')
                 //Boolean - Whether to fill the dataset with a colour
                 datasetFill: true,
 
+                multiTooltipTemplate: "<%= datasetLabel %> - <%= value %>",
                 //String - A legend template
-                legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+                legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"><%if(datasets[i].label){%><%=datasets[i].label%><%}%></span></li><%}%></ul>"
             };
 
             var g_data = {
                 labels: ["January", "February", "March", "April", "May", "June", "July"],
                 datasets: [{
-                    label: "My First dataset",
+                    label: "Average Response Time (ms)",
                     fillColor: "rgba(220,220,220,0.2)",
                     strokeColor: "rgba(220,220,220,1)",
                     pointColor: "rgba(220,220,220,1)",
                     pointStrokeColor: "#fff",
                     pointHighlightFill: "#fff",
                     pointHighlightStroke: "rgba(220,220,220,1)",
-                    data: [65, 59, 80, 81, 56, 55, 40]
+                    data: [65, 59, 80, 81, 56, 100, 40, 100, 100, 100]
                 }, {
-                    label: "My Second dataset",
+                    label: "Requests Per Minute",
                     fillColor: "rgba(151,187,205,0.2)",
                     strokeColor: "rgba(151,187,205,1)",
                     pointColor: "rgba(151,187,205,1)",
@@ -75,40 +77,56 @@ angular.module('orderRequest')
                 }]
             };
 
+            function refreshChart() {
+                $.ajax({
+                    url: "https://api.newrelic.com/v2/applications/7651145/metrics/data.json?names[]=Agent/MetricsReported/count",
+                    data: {},
+                    type: "GET",
+                    beforeSend: function(xhr) {
+                        xhr.setRequestHeader('X-Api-Key', '49dc6116b6cfd1142dc3793ac334b146b28bacb3b39fd77');
+                    },
+                    success: function(data) {
+                        console.log(data);
+                        g_data.labels = [];
+                        g_data.datasets[0].data = [];
+                        g_data.datasets[1].data = [];
+                        var timeslices = data.metric_data.metrics[0].timeslices;
 
 
+                        for (var i = timeslices.length - 10; i < timeslices.length - 1; i++) {
+                            g_data.labels.push(moment(timeslices[i].from).format('hh:mm'));
+                            g_data.datasets[0].data.push(timeslices[i].values.average_response_time);
+                            g_data.datasets[1].data.push(timeslices[i].values.requests_per_minute);
+                        }
+                        console.log(g_data);
 
 
+                        // This will get the first returned node in the jQuery collection.
+                        var myLineChart = new Chart(ctx).Line(g_data, options);
+                        document.getElementById("legend").innerHTML = myLineChart.generateLegend();
 
-            // Get context with jQuery - using jQuery's .get() method.
-            var ctx = $("#myChart").get(0).getContext("2d");
-            $.ajax({
-                url: "https://api.newrelic.com/v2/applications/7651145/metrics/data.json?names[]=Agent/MetricsReported/count",
-                data: {},
-                type: "GET",
-                beforeSend: function(xhr) {
-                    xhr.setRequestHeader('X-Api-Key', '49dc6116b6cfd1142dc3793ac334b146b28bacb3b39fd77');
-                },
-                success: function(data) {
-                    console.log(data);
-                    alert('Success!');
-
-                    g_data.labels = [];
-                    g_data.datasets[0].data = [];
-                    var timeslices = data.metric_data.metrics[0].timeslices;
-
-
-                    for (var i = 0; i < timeslices.length - 1; i++) {
-                        g_data.labels.push(timeslices[i].from);
-                        g_data.datasets[0].data.push(timeslices[i].values.average_response_time);
                     }
-                    console.log(g_data);
+                });
+            }
 
+            function chartRefreshLoop() {
 
-                    // This will get the first returned node in the jQuery collection.
-                    var myLineChart = new Chart(ctx).Line(g_data, options);
-
+                $scope.chartRefreshTime -= 1;
+                if ($scope.chartRefreshTime === 0) {
+                    refreshChart();
+                    $scope.chartRefreshTime = 60;
                 }
-            });
+
+                console.log("running");
+                setTimeout(function() {
+                    $scope.$apply(function() {
+                        chartRefreshLoop();
+                    });
+                }, 1000);
+            }
+            refreshChart();
+            chartRefreshLoop();
+
+
         }
     ]);
